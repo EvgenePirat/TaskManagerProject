e74b7cfc37a0507cc01a23c24cfe200c02718c63
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-04-10 11:37:27 +0300

    I mae model and validation for entity!

--- src/main/java/com/example/taskmanagerproject/dto/model/UserDto.java
+++ src/main/java/com/example/taskmanagerproject/dto/model/UserDto.java
@@ -6,7 +6,10 @@ import com.example.taskmanagerproject.entity.user.Role;
 import com.example.taskmanagerproject.entity.task_user.TaskUser;
 import com.example.taskmanagerproject.entity.user.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
@@ -17,19 +20,20 @@ import java.util.Set;
 @AllArgsConstructor
 public class UserDto {
 
+    @Positive(message = "Id must be positive number!",groups = OnUpdate.class)
     @NotNull(message = "Id must be not null!", groups = OnUpdate.class)
     private Long id;
 
-    @NotNull(message = "Username must be not null!", groups = {OnCreate.class, OnUpdate.class})
+    @NotNull(message = "Username must be not null!", groups = OnCreate.class)
+    @Email(message = "Username must be email!", groups = OnCreate.class)
     @Length(max = 500, message = "Username must be smaller than 500 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String username;
 
-    @NotNull(message = "Name must be not null!", groups = {OnUpdate.class})
-    @Length(max = 255, message = "Name must be smaller than 255 symbols", groups = {OnUpdate.class})
+    @Length(max = 255, message = "Name must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String name;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @NotNull(message = "Password must be not null!", groups = {OnCreate.class, OnUpdate.class})
+    @NotNull(message = "Password must be not null!", groups = OnCreate.class)
     @Length(max = 500, message = "Password must be smaller than 500 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String password;
 
@@ -38,6 +42,7 @@ public class UserDto {
 
     private List<TaskUser> userList;
 
+    @NotEmpty(message = "User can have role!", groups = OnCreate.class)
     private Set<Role> roles;
 
     public static UserDto changeFromUserToUserDto(User user){
