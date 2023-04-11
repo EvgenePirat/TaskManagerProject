402c7b74aecbfa3eb90d274690a58041585c88f1
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-04-11 10:26:23 +0300

    i add spring boot email!

--- src/main/java/com/example/taskmanagerproject/security/config/ApplicationConfig.java
+++ src/main/java/com/example/taskmanagerproject/security/config/ApplicationConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
